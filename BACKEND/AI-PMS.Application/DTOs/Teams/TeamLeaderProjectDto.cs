e8040ed69cc7cd11f7ed6727257538c77cdd1a06
Leantan23 <148032508+Leantan23@users.noreply.github.com>
2026-09-01 18:41:41 +0300

    Update project and team leader backend

--- BACKEND/AI-PMS.Application/DTOs/Teams/TeamLeaderProjectDto.cs
+++ BACKEND/AI-PMS.Application/DTOs/Teams/TeamLeaderProjectDto.cs
@@ -4,33 +4,27 @@ namespace AI_PMS.Application.DTOs.Teams
     {
         public Guid Id { get; set; }
 
-        public string Name { get; set; }
-            = string.Empty;
+        public string Name { get; set; } = string.Empty;
 
-        public string Description { get; set; }
-            = string.Empty;
+        public string Description { get; set; } = string.Empty;
 
         public Guid StatusId { get; set; }
 
-        public string StatusName { get; set; }
-            = string.Empty;
+        public string StatusName { get; set; } = string.Empty;
 
         public decimal ProgressPercentage { get; set; }
 
         public Guid? TeamId { get; set; }
 
-        public string TeamName { get; set; }
-            = string.Empty;
+        public string TeamName { get; set; } = string.Empty;
 
         public Guid? ManagerId { get; set; }
 
-        public string ManagerName { get; set; }
-            = string.Empty;
+        public string ManagerName { get; set; } = string.Empty;
 
         public int PriorityId { get; set; }
 
-        public string PriorityName { get; set; }
-            = string.Empty;
+        public string PriorityName { get; set; } = string.Empty;
 
         public DateTime StartDate { get; set; }
 
